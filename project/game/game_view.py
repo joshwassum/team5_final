61c8d9703eb2e0130b13b80ca931609262e3d534
Joshua Wassum <37701220+joshwassum@users.noreply.github.com>
2021-11-24 12:34:07 -0700

    Revert "Revert "added map for level - added jump sound""

--- project/game/game_view.py
+++ project/game/game_view.py
@@ -2,6 +2,8 @@ import arcade
 from game import constants
 from game.control_sprites_action import ControlSpritesAction
 from game.handle_collisions_action import HandleCollisionsAction
+from game.constants import LAYER_NAME_PLATFORMS
+from game.constants import LAYER_NAME_PLAYER
 
 class Game_View(arcade.View):
     """Creates our game screen and sets up the elements on screen. Uses the Window functions built into
@@ -36,6 +38,12 @@ class Game_View(arcade.View):
         self.physics_engine = None
         self.gui_camera = None
         self._setup()
+        self.coin_collect_sound = arcade.load_sound(constants.COIN_COLLISION_SOUND)
+        self.jump_sound = arcade.load_sound(constants.PLAYER_JUMP_SOUND)
+
+        self.tile_map = None
+        self.end_of_map = 0
+        self.level = 1
 
     def on_draw(self):
         """Renders the screen.
@@ -58,12 +66,12 @@ class Game_View(arcade.View):
             self (Game_Window): An instance of the Game_Window object
         """
         press = True
-        self.movement_engine.execute(self.scene, key, self.physics_engine,press)
+        self.movement_engine.execute(self.scene, key, self.physics_engine,press, self.jump_sound)
 
     def on_key_release(self, key, modifiers):
         """Called when the user releases a key."""
         press = False
-        self.movement_engine.execute(self.scene,key, self.physics_engine, press)
+        self.movement_engine.execute(self.scene,key, self.physics_engine, press, self.jump_sound)
 
 
     def center_camera_to_player(self):
@@ -91,10 +99,12 @@ class Game_View(arcade.View):
             self (Game_Window): An instance of the Game_Window object.
         """
         self.physics_engine = arcade.PhysicsEnginePlatformer(
-            self.scene["Player"][0], gravity_constant=constants.GRAVITY, walls=self.scene["Platform"]
+            self.scene["Player"][0], gravity_constant=constants.GRAVITY, walls=self.scene[LAYER_NAME_PLATFORMS]
         )
         self.camera = arcade.Camera(self.window.width, self.window.height)
         self.gui_camera = arcade.Camera(self.window.width, self.window.height)
+
+
     
     def on_update(self, delta_time):
         """Movement and game logic
@@ -103,5 +113,6 @@ class Game_View(arcade.View):
             self (Game_Window): An instance of the Game_Window object.
             delta_time (Time): An instance of time.
         """
-        self.collision_engine.execute(self.scene)
+        self.collision_engine.execute(self.scene, self.coin_collect_sound)
         self.physics_engine.update()
+        self.center_camera_to_player()
